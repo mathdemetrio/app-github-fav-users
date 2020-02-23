f3f09f62ebb8e6fa48211ff93862774be865c760
Matheus Demetrio <matheus.fdemetrio@gmail.com>
2020-02-22 21:04:28 -0300

    feat: initial config and routes

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        gestureEnabled: false,
+        headerBackTitleVisible: false,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#ffffff',
+      }}
+    >
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ title: 'Usuários' }}
+      />
+      <Stack.Screen name="User" component={User} />
+    </Stack.Navigator>
+  );
+}
+
+export default function Routes() {
+  return <NavigationContainer>{RootStack()}</NavigationContainer>;
+}
