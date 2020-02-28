dfff4648dac2396fa620ec8abd3154d793188697
Matheus Demetrio <matheus.fdemetrio@gmail.com>
2020-02-27 22:20:53 -0300

    feat: display alert when user is not found

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard, ActivityIndicator } from 'react-native';
+import { Keyboard, ActivityIndicator, Alert } from 'react-native';
 import PropTypes from 'prop-types';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -51,22 +51,29 @@ export default class Main extends Component {
   handlerAddUser = async () => {
     const { users, newUser } = this.state;
 
-    this.setState({ loading: true });
+    try {
+      this.setState({ loading: true });
 
-    const response = await api.get(`/users/${newUser}`);
+      const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
 
-    this.setState({
-      users: [...users, data],
-      newUser: '',
-      loading: false,
-    });
+      this.setState({
+        users: [...users, data],
+        newUser: '',
+      });
+    } catch (error) {
+      this.handleError();
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
 
     Keyboard.dismiss();
   };
@@ -74,6 +81,10 @@ export default class Main extends Component {
   handleNavigate = user => {
     const { navigation } = this.props;
     navigation.navigate('User', { user });
+  };
+
+  handleError = () => {
+    Alert.alert('Usuário não encontrado!');
   };
 
   render() {
